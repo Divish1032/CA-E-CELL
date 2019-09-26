d411d4a2947667fcaa075ceda2d71ff54a873475
Divish1032 <kumar.divansh1996@gmail.com>
2019-09-26 08:26:22 +0530

    f

--- public/js/script.js
+++ public/js/script.js
@@ -2,28 +2,28 @@
 
 
 $(document).ready(function() {
-    $('.1 a').click(function(e) {
+    $('.1').click(function(e) {
       e.preventDefault();
   
       $('html, body').animate({
         scrollTop: $('.second').offset().top
       }, 500);
     });
-    $('.2 a').click(function(e) {
+    $('.2').click(function(e) {
         e.preventDefault();
     
         $('html, body').animate({
           scrollTop: $('.fourth').offset().top
         }, 500);
     });
-    $('.3 a').click(function(e) {
+    $('.3 ').click(function(e) {
         e.preventDefault();
 
         $('html, body').animate({
             scrollTop: $('.seventh h1').offset().top
         }, 500);
     });
-    $('.4 a').click(function(e) {
+    $('.4').click(function(e) {
         e.preventDefault();
     
         $('html, body').animate({
