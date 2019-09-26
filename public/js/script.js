81327b4c953462eb33c7982dd3ca633e593900a0
Divish1032 <kumar.divansh1996@gmail.com>
2019-09-26 07:57:45 +0530

    f

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,34 @@
-const btn = document.getElementById("btn");
-const second = document.getElementById("second");
 
-let showCard = (event) => {
-    btn.classList.toggle("is-rotate");
-    second.classList.toggle("is-visible");
-}
 
-btn.addEventListener("click", showCard);
+
+$(document).ready(function() {
+    $('.1 a').click(function(e) {
+      e.preventDefault();
+  
+      $('html, body').animate({
+        scrollTop: $('.second').offset().top
+      }, 500);
+    });
+    $('.2 a').click(function(e) {
+        e.preventDefault();
+    
+        $('html, body').animate({
+          scrollTop: $('.fourth').offset().top
+        }, 500);
+    });
+    $('.3 a').click(function(e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $('.seventh h1').offset().top
+        }, 500);
+    });
+    $('.4 a').click(function(e) {
+        e.preventDefault();
+    
+        $('html, body').animate({
+          scrollTop: $('.sixth').offset().top
+        }, 500);
+      });
+  });
+
