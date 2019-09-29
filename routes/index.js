1939686df474bc1c69ec2e66e531a31ef295ae5e
Divish1032 <kumar.divansh1996@gmail.com>
2019-09-30 00:41:58 +0530

    Fe

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,15 @@ router.get('/', (req, res) => {
 })
 
 
-/* router.get('/dashboard', middleware.ensureAuthenticated, (req, res) => {
+router.get('/dashboard', middleware.ensureAuthenticated, (req, res) => {
     if(req.user.verified== false){
-      req.flash('error_msg', 'Please verify the email id');
+        req.flash('error_msg', 'Please verify the email id');
     }
     res.render('dashboard', {
-      user: req.user
+        user: req.user
     })
-  }
-  ); */
-
-
+    }
+);
 
   // Login Page
 router.get('/users/login', middleware.forwardAuthenticated, (req, res) =>{
@@ -33,9 +31,60 @@ router.get('/users/login', middleware.forwardAuthenticated, (req, res) =>{
 
 
 
+// Register
+router.post('/register', (req, res) => {
+    const { name, email, password, password2, phone, college } = req.body;
+    let errors = [];
+    if (password != password2) {
+      errors.push({ msg: 'Passwords do not match' });
+    }
+  
+    if (password.length < 6) {
+      errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+  
+    if (errors.length > 0) {
+      res.render('register', { errors, name, email, password, password2, phone, college });
+    } 
+    else {
+      User.findOne({ email: email }).then(user => {
+        if (user) {
+          errors.push({ msg: 'Email already exists' });
+          res.render('register', { errors, name, email, password, password2 });
+        } 
+        else {
+          var newUser = new User({ name, email, password, phone, college });
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser.save().then(user => {
+                  req.flash('success_msg','You are now registered and can log in');
+                  res.redirect('/users/login');
+                })
+                .catch(err => console.log(err));
+            });
+          });
+        }
+      });
+    }
+  });
+
+// Login
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+      successRedirect: '/dashboard',
+      failureRedirect: '/users/login',
+      failureFlash: true
+    })(req, res, next);
+  });
+
+
+
 router.get("/logout", function(req, res) {
-    console.log(req.user);
+    
     req.logout(); 
+    req.flash('success_msg', 'You are logged out');
     res.redirect("/");
  });
 
