4032f19886b4d44eb984900fda538be1881eacdb
Divish1032 <kumar.divansh1996@gmail.com>
2019-09-30 00:49:37 +0530

    Fe5

--- models/User.js
+++ models/User.js
@@ -1,41 +1,14 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  college: {
-     type: String,
-     required: true
-  },
-  phone: {
-     type: String,
-     required: true 
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  verified:{
-    type: Boolean,
-    default: false
-  },
-  referal_code: {
-    type : String,
-    default : null
-  }
-});
+var userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    college: { type: String, required: true },
+    phone: { type: String, required: true }
+  });
 
-  UserSchema.plugin(passportLocalMongoose);
+  userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
